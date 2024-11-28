080010d55acb4392cdfc711f49a42278ad015e98
Elidnaya <romain.massa@laplateforme.io>
2024-11-28 15:07:24 +0100

    Derniere MAJ

--- password/main.py
+++ password/main.py
@@ -6,6 +6,7 @@ def verification_mot_de_passe(mot_de_passe):
         print("Le mot de passe doit avoir 8 caractères minimum.")
         return False
     if not any(c.isupper() for c in mot_de_passe):
+        
         print("Le mot de passe doit contenir au moins une lettre majuscule.")
         return False
     if not any(c.islower() for c in mot_de_passe):
@@ -20,12 +21,12 @@ def verification_mot_de_passe(mot_de_passe):
         return False
     return True
 
-mot_de_passe=input("Veuillez saisir un mot de passe : ")
+mot_de_passe = input("Veuillez saisir un mot de passe : ")
 while not verification_mot_de_passe(mot_de_passe):
     mot_de_passe = input("Le mot de passe est invalide veuillez saisir un mot de passe : ")
 
 if verification_mot_de_passe(mot_de_passe):
-    print("mot de passe valide") 
+    print("mot de passe valide")
 
 
 def hacher_mot_de_passe(mot_de_passe):
@@ -34,16 +35,12 @@ def hacher_mot_de_passe(mot_de_passe):
     sha256.update(mot_de_passe.encode('utf-8'))
     return sha256.hexdigest()
 
-
-mot_de_passe = input("Veuillez saisir un mot de passe : ")
-
 while not verification_mot_de_passe(mot_de_passe):
     mot_de_passe = input("Le mot de passe est invalide, veuillez réessayer : ")
 
 hash_mot_de_passe = hacher_mot_de_passe(mot_de_passe)
 print("\nMot de passe valide !")
 print("Hash SHA-256 du mot de passe :", hash_mot_de_passe)
-
 
 
 
